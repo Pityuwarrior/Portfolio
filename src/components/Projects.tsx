d3da965d595dd6fd6f0aee450530ff2d6a03610a
Pityuwarrior <nove.norbert2002@gmail.com>
2024-09-16 18:55:04 +0200

    10.09.16 Reworking About Me section.

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,19 +1,12 @@
-import variables from '../assets/scss/About_me.module.scss';
+import variables from '../assets/scss/Projects.module.scss';
 import staticText from '../data/contentHome.json'; 
 import image from '../../public/me.png';
 
 
 export function Projects(){
     return(
-        <section className = {`${variables.home_section} protfolio_section`} id = "projects">
-                <div className = {variables.left_container}>
-                    <p className = {variables.home_greeting}>{staticText.title}</p>
-                    <p className = {variables.home_name}>{staticText.description}</p>
-                    <p className = {variables.home_mytitle}>{staticText.content}</p>                  
-                </div>
-                <div className = {variables.right_container}>
-                    <img className={variables.image_aboutme} src={image} alt="Me.png"/>
-                </div>      
+        <section className = {`${variables.projects_section} protfolio_section`} id = "projects">
+                
         </section>
     )
 }
