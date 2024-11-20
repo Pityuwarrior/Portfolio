5774783bccf873edea4b391e66f75f240a2ba15a
Pityuwarrior <nove.norbert2002@gmail.com>
2024-11-20 02:19:59 +0100

    19.11.24 Skill container improvement.

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,10 +1,13 @@
 import variables from '../assets/scss/Contact.module.scss';
-
+import staticText from '../data/contentContact.json';
 
 export function Contact(){
     return(
         <section className = {`${variables.contact_section} protfolio_section`}id = "contact">
-                  
+            <div className={variables.contact_text}>   
+                <h1 className = {variables.contact_title}>{staticText.title}</h1>
+                <h2 className = {variables.contact_description}>{staticText.description}</h2>
+            </div>
         </section>
     )
 }
